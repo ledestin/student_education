eb8501f32bc46d13f1cf94e1acbdc667d68ae8bc
Dmitry Maksyoma <ledestin@gmail.com>
2018-07-27 07:04:44 +1200

    Add home controller to render /

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
   get "/students", to: "students#index"
   get "/students/:id", to: "students#show"
   put "/students/:id", to: "students#update"
