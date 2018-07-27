e448f851b035b45f314eddbf9383d320585d675a
Dmitry Maksyoma <ledestin@gmail.com>
2018-07-27 21:50:00 +1200

    JS: render App as a React component

--- app/javascript/src/js/app.js
+++ app/javascript/src/js/app.js
@@ -1,15 +1,22 @@
+import $ from 'jquery'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Backbone from 'backbone'
 import Students from './backbone/collections/students.js'
 
-export class App {
-  constructor() {
+export class App extends React.Component {
+  constructor(props) {
+    super(props)
+
     this.students = new Students()
     this.students.fetch().then(() => console.log(this.students.get(1)))
   }
 
-  hello = () => {
-    console.log("Hello from App")
+  render = () => {
+    return <div>Hello from App</div>
   }
 }
 
-new App().hello()
+$(function() {
+  ReactDOM.render(<App />, document.getElementById('app'))
+})
