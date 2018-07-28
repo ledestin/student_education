5e05634543d4545335c7485b7bf6c0d6ac5cfab3
Dmitry Maksyoma <ledestin@gmail.com>
2018-07-28 23:19:19 +1200

    UI: show a list of teachers on home page

--- app/javascript/src/js/app.js
+++ app/javascript/src/js/app.js
@@ -4,24 +4,35 @@ import ReactDOM from 'react-dom'
 import Backbone from 'backbone'
 
 import Students from './backbone/collections/students.js'
+import Teachers from './backbone/collections/teachers.js'
 import { StudentGrid } from './student_grid.js'
+import CollectionGrid from './collection_grid.js'
 
 export class App extends React.Component {
   constructor(props) {
     super(props)
 
     let students = new Students()
-    this.state = { students }
+    let teachers = new Teachers()
+    this.state = { students, teachers }
   }
 
   componentDidMount() {
     this.state.students.fetch()
       .then(() => this.setState({}))
       .catch(error => console.log(error))
+    this.state.teachers.fetch()
+      .then(() => this.setState({}))
+      .catch(error => console.log(error))
   }
 
   render() {
-    return <StudentGrid collection={this.state.students} />
+    return (
+      <div>
+        <CollectionGrid title="Teachers" collection={this.state.teachers} />
+        <StudentGrid collection={this.state.students} />
+      </div>
+    )
   }
 }
 
