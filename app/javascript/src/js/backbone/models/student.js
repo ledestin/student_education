ecd62eeb32550f36f49fac3b775c63d373a43768
Dmitry Maksyoma <ledestin@gmail.com>
2018-07-30 07:16:12 +1200

    JS: add virtual student attribute completed_lesson_and_part

--- app/javascript/src/js/backbone/models/student.js
+++ app/javascript/src/js/backbone/models/student.js
@@ -1,3 +1,20 @@
-const Student = Backbone.Model.extend({})
+const Student = Backbone.Model.extend({
+  constructor: function() {
+    this.on('change:completed_lesson change:completed_part', () => {
+      this.set("completed_lesson_and_part", this.completedLessonAndPart())
+    })
+    Backbone.Model.apply(this, arguments)
+  },
+
+  completedLessonAndPart: function() {
+    const lesson = this.get('completed_lesson')
+    const part = this.get('completed_part')
+
+    if (!lesson)
+      return null
+
+    return `Lesson ${lesson}, part ${part}`
+  }
+})
 
 export default Student
