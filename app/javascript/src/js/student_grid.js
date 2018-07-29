12feabeae92ffd3b744e4c8c7dcaf09141750c20
Dmitry Maksyoma <ledestin@gmail.com>
2018-07-30 07:27:56 +1200

    UI: show student progress in teacher's student report

--- app/javascript/src/js/student_grid.js
+++ app/javascript/src/js/student_grid.js
@@ -3,8 +3,28 @@ import React from 'react'
 import CollectionGrid from './collection_grid.js'
 
 export default class StudentGrid extends React.Component {
+  get columns() {
+    return [
+      {
+        key: 'id',
+        name: 'ID',
+        width: 35
+      },
+      {
+        key: 'name',
+        name: 'Name',
+        width: 300
+      },
+      {
+        key: 'completed_lesson_and_part',
+        name: 'Completed Lesson and Part',
+        width: 300
+      },
+    ]
+  }
+
   render() {
-    return <CollectionGrid title="Students"
+    return <CollectionGrid title="Students" columns={this.columns}
       collection={this.props.collection} />
   }
 }
