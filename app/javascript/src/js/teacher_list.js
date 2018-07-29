b4622dd8f830c1a2e907c3f5051ac3424bcf5c33
Dmitry Maksyoma <ledestin@gmail.com>
2018-07-29 22:08:52 +1200

    JS refacoring: extract TeacherList.linkToStudentList

--- app/javascript/src/js/teacher_list.js
+++ app/javascript/src/js/teacher_list.js
@@ -3,21 +3,25 @@ import { Link } from 'react-router-dom'
 
 export default class TeacherList extends React.Component {
   render() {
-    const teachers = this.props.collection.map((teacher) => {
-      const teacher_id = teacher.get('id')
-
-      return (
-        <Link key={teacher_id} to={`/student_list?teacher_id=${teacher_id}`}>
-          {teacher.get('name')}
-        </Link>
-      )
-    })
-
     return (
       <div>
         <label>{this.props.title}</label>
-        {teachers}
+        {this.teachers()}
       </div>
+    )
+  }
+
+  teachers() {
+    return this.props.collection.map(this.linkToStudentList)
+  }
+
+  linkToStudentList(teacher) {
+    const teacherId = teacher.get('id')
+
+    return (
+      <Link key={teacherId} to={`/student_list?teacher_id=${teacherId}`}>
+        {teacher.get('name')}
+      </Link>
     )
   }
 }
