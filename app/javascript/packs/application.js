ff10667bfbfb1c0a8f630bf90f7d58ee838f7bf3
Dmitry Maksyoma <ledestin@gmail.com>
2018-08-01 07:00:07 +1200

    JS: ensure features specs can see React

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import 'babel-polyfill'
 import $ from 'jquery'
 import React from 'react'
 import ReactDOM from 'react-dom'
