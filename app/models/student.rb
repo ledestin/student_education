dd13fa3957d3010c367d6ecf805af23ded883981
Dmitry Maksyoma <ledestin@gmail.com>
2018-08-01 22:59:57 +1200

    Add Student.completed_lesson and completed_part validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,12 @@
 class Student < ApplicationRecord
   belongs_to :teacher, required: false
+
+  validates_inclusion_of :completed_lesson, in: 1..100, allow_nil: true
+  validates_inclusion_of :completed_part, in: 1..3, allow_nil: true
+  validates_each :completed_lesson do |record, attr, value|
+    next if record.completed_lesson && record.completed_part
+    next if record.completed_lesson.nil? && record.completed_part.nil?
+
+    record.errors.add attr, "and completed_part must be both set or both nil"
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Student, type: :model do
   it { is_expected.to belong_to(:teacher) }
+  it { is_expected.to validate_inclusion_of(:completed_lesson).in_range(1..100) }
+  it { is_expected.to validate_inclusion_of(:completed_part).in_range(1..3) }
+
+  it "fails validation if completed_lesson is set and completed_part isn't" do
+    subject.completed_lesson = 1
+
+    expect(subject).to have(1).errors_on(:completed_lesson)
+  end
+
+  it "fails validation if completed_part is set and completed_lesson isn't" do
+    subject.completed_part = 1
+
+    expect(subject).to have(1).errors_on(:completed_lesson)
+  end
 end
