7e984b7177d1b1467e60ecfd7a3004133c6df568
Dmitry Maksyoma <ledestin@gmail.com>
2018-08-03 00:17:20 +1200

    API: RecordNotFound exceptions result in :bad_request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render json: { errors: [exception.message] }, status: :bad_request
+  end
 end
