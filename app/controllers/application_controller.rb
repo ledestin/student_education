0a69f80368bfd6e926b20ea38bf0f6d9e7025d18
Dmitry Maksyoma <ledestin@gmail.com>
2018-08-03 00:18:41 +1200

    API: make it accessible by clients like Insomnia

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: { errors: [exception.message] }, status: :bad_request
   end
